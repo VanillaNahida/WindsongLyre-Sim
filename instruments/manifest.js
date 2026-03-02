9dda28eecff5225f9b66fae7ed46dfcc659214c1
苍二君 <527254719@qq.com>
2026-03-02 18:01:27 +0800

    Update manifest.js

--- instruments/manifest.js
+++ instruments/manifest.js
@@ -1,58 +1,58 @@
 let insts = {
-    "windsong_lyre": {
-        "id": "windsong_lyre",
+    "Lyre": {
+        "id": "Lyre",
         "name": "风物之诗琴",
-        "color": "rgb(88, 153, 144)",
-        "bg1": "rgb(144, 249, 227)",
-        "bg2": "rgba(144, 249, 227, 0.8)"
+        "color": "#589990",
+        "bg1": "#90F9E3",
+        "bg2": "#90F9E3CC"
     },
-    "floral_zither": {
-        "id": "floral_zither",
+    "Zither": {
+        "id": "Zither",
         "name": "镜花之琴",
-        "color": "rgb(173, 159, 146)",
-        "bg1": "rgb(221, 203, 161)",
-        "bg2": "rgba(221, 203, 161, 0.8)"
+        "color": "#AD9F92",
+        "bg1": "#DDCBA1",
+        "bg2": "#DDCBA1CC"
     },
-    "floral_zither_old": {
-        "id": "floral_zither_old",
+    "Old-Zither": {
+        "id": "Old-Zither",
         "name": "镜花之琴(旧版)",
-        "color": "rgb(173, 159, 146)",
-        "bg1": "rgb(221, 203, 161)",
-        "bg2": "rgba(221, 203, 161, 0.8)"
+        "color": "#AD9F92",
+        "bg1": "#DDCBA1",
+        "bg2": "#DDCBA1CC"
     },
-    "vintage_lyre": {
-        "id": "vintage_lyre",
+    "Vintage-Lyre": {
+        "id": "Vintage-Lyre",
         "name": "老旧的诗琴",
-        "color": "rgb(127, 179, 99)",
-        "bg1": "rgb(160, 216, 128)",
-        "bg2": "rgba(160, 216, 128, 0.8)"
+        "color": "#7FB363",
+        "bg1": "#A0D880",
+        "bg2": "#A0D880CC"
     },
     "Ukulele": {
         "id": "Ukulele",
         "name": "悠可琴",
-        "color": "rgb(255, 204, 153)",
-        "bg1": "rgb(255, 238, 204)",
-        "bg2": "rgba(255, 238, 204, 0.8)"
+        "color": "#FFCC99",
+        "bg1": "#FFEECC",
+        "bg2": "FFEECCCC"
     },
-    "lingering_Euphonia": {
-        "id": "lingering_Euphonia",
+    "LingeringEuphonia": {
+        "id": "lingeringEuphonia",
         "name": "「余音」",
-        "color": "rgb(146,136,211)",
-        "bg1": "rgb(149,145,223)",
-        "bg2": "rgb(149,145,223, 0.8)"
+        "color": "#9288D3",
+        "bg1": "#9591DF",
+        "bg2": "#9591DFCC"
     },
-    "leaping_spirit_piano": {
-        "id": "leaping_spirit_piano",
-        "name": "跃律琴",
-        "color": "rgb(83, 175, 193)",
-        "bg1": "rgb(83, 170, 182)",
-        "bg2": "rgba(102, 182, 188, 0.8)"
-    },
-    "Harmonic_Keys": {
-        "id": "Harmonic_Keys",
+    "HarmonicKey": {
+        "id": "HarmonicKey",
         "name": "谐律键琴",
-        "color": "rgb(221, 176, 85)",
-        "bg1": "rgb(241, 196, 105)",
-        "bg2": "rgba(241, 196, 105, 0.8)"
+        "color": "#DDB055FF",
+        "bg1": "#E1CBA3FF",
+        "bg2": "#E1CBA3CC"
+    },
+    "LeapingSpiritPiano": {
+        "id": "LeapingSpiritPiano",
+        "name": "跃律琴",
+        "color": "#5CADBDFF",
+        "bg1": "#5CADBDFF",
+        "bg2": "#5CADBDCC"
     }
 };
